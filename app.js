346befc49ad40e816e31a5e1521ab7480e18a2e5
Kirill <krlktv@mail.ru>
2021-07-23 16:06:23 +0300

    refactor: use template literals

--- app.js
+++ app.js
@@ -123,19 +123,19 @@ function toTaskDate(taskDate) {
 		year = date.getFullYear();
 
 	if (hours < 10) {
-		hours = '0' + hours;
+		hours = `0${hours}`;
 	}
 
 	if (minutes < 10) {
-		minutes = '0' + minutes;
+		minutes = `0${minutes}`;
 	}
 
 	if (day < 10) {
-		day = '0' + day;
+		day = `0${day}`;
 	}
 
 	if (month < 10) {
-		month = '0' + month;
+		month = `0${month}`;
 	}
 	return `${hours}:${minutes} ${day}.${month}.${year}`;
 }
