98504dc864542f576329b95dd42b8a8ea912d3c0
Kirill Kotov <krlktv@mail.ru>
2021-07-26 14:33:38 +0300

    refactor: add reusable methods and ternary operator

--- app.js
+++ app.js
@@ -177,37 +177,13 @@ function renderTasks() {
 				color = 'bg-danger';
 			}
 
-			let editBtn = '<button type="button" class="btn btn-info w-100" data-toggle="modal" data-target="#editModal">Edit</button>';
-			if (current.isCompleted) {
-				editBtn = '';
-			}
+			const editButton = '<button type="button" class="btn btn-info w-100" data-toggle="modal" data-target="#editModal">Edit</button>';
+			const editBtn = !current.isCompleted ? editButton : '';
 
 			const task = document.createElement('li');
 			task.className = createTaskClassName(color);
 			task.dataset.id = current.id;
-			task.innerHTML = `
-			<div class="w-100 mr-2">
-				<div class="d-flex w-100 justify-content-between">
-					<h5 class="mb-1">${current.title}</h5>
-					<div>
-						<small class="mr-2">${current.priority} priority</small>
-						<small>${toTaskDate(new Date(current.date))}</small>
-					</div>
-
-				</div>
-				<p class="mb-1 w-100">${current.text}</p>
-			</div>
-			<div class="dropdown m-2 dropleft">
-				<button class="btn btn-secondary h-100" type="button" id="dropdownMenuItem1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-					<i class="fas fa-ellipsis-v" aria-hidden="true"></i>
-				</button>
-				<div class="dropdown-menu p-2 flex-column" aria-labelledby="dropdownMenuItem1">
-					<button type="button" class="btn btn-success w-100 mb-2">Toggle completeness</button>
-					${editBtn}
-					<button type="button" class="btn btn-danger w-100 mt-2">Delete</button>
-				</div>
-			</div>
-		`;
+			task.innerHTML = createTaskTemplate(current.title, current.priority, current.date, current.text, editBtn);
 
 			if (!current.isCompleted) {
 				currentTasks.appendChild(task);
@@ -229,11 +205,7 @@ function deleteTask(id) {
 
 function toggleCompleteness(id) {
 	const task = JSON.parse(localStorage.getItem(id));
-	if (!task.isCompleted) {
-		task.isCompleted = true;
-	} else {
-		task.isCompleted = false;
-	}
+	task.isCompleted = !task.isCompleted ? true : false;
 	localStorage.setItem(id, JSON.stringify(task));
 }
 
@@ -278,4 +250,30 @@ function getTaskId(elem) {
 
 function createTaskClassName(color) {
 	return `list-group-item d-flex w-100 mb-2 ${color}`;
+}
+
+function createTaskTemplate(title, priority, date, text, editBtn) {
+	return `
+		<div class="w-100 mr-2">
+			<div class="d-flex w-100 justify-content-between">
+				<h5 class="mb-1">${title}</h5>
+				<div>
+					<small class="mr-2">${priority} priority</small>
+					<small>${toTaskDate(new Date(date))}</small>
+				</div>
+
+			</div>
+			<p class="mb-1 w-100">${text}</p>
+		</div>
+		<div class="dropdown m-2 dropleft">
+			<button class="btn btn-secondary h-100" type="button" id="dropdownMenuItem1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				<i class="fas fa-ellipsis-v" aria-hidden="true"></i>
+			</button>
+			<div class="dropdown-menu p-2 flex-column" aria-labelledby="dropdownMenuItem1">
+				<button type="button" class="btn btn-success w-100 mb-2">Toggle completeness</button>
+				${editBtn}
+				<button type="button" class="btn btn-danger w-100 mt-2">Delete</button>
+			</div>
+		</div>
+	`;
 }
